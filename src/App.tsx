3fcc80335498d1656f98ed2977462c41cb5c3a2e
Robson Bittencourt <rob.limab@gmail.com>
2021-06-15 22:28:38 -0300

    Feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
 
 interface GenreResponseProps {
   id: number;
@@ -29,6 +23,14 @@ interface MovieProps {
   }>;
   Runtime: string;
 }
+
+interface SelectedMovie {
+  handleClickButton: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export const SelectedCategoryContext = createContext({} as SelectedMovie);
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
@@ -60,36 +62,17 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+      <SelectedCategoryContext.Provider value={{ genres, handleClickButton, selectedGenreId }}>
+        <SideBar />
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
+        <div className="container">
+          <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          </header>
+
+          <Content movies={movies} />
         </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      </SelectedCategoryContext.Provider>
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
+import { useContext } from 'react';
+import { SelectedCategoryContext } from "../App"
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = useContext(SelectedCategoryContext);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
